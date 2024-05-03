3f9e3482276e312b961e1779fd9acd69abc10433
// Sudhanshu <keysatmyfingertips@yahoo.com>
2024-05-03 12:46:00 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// Import the required packages
+import java.io.BufferedReader; // Reads text from a character-input stream, buffering characters 
+import java.io.BufferedWriter;
+import java.io.FileReader; // Convenience class for reading character files
+import java.io.FileWriter;
+import java.io.IOException; // Convenience class for Input/Output exceptions 
+import java.util.ArrayList; // List class in the form of arrays
+import java.util.Arrays;	// Class for handling Array Like structures
+import java.util.List;		// Class for creating List Structures
+
+import datastructures.*;
+
+// Main class for getting data and executing the algorithms
+public class Main {
+	// Entry point for java compiler
+	public static void main(String[] args) throws IOException {
+		
+	
+
+
+		// Execution done
+		System.out.println("\nExecution completed successfully!");
+	}
+	
+	
+}
